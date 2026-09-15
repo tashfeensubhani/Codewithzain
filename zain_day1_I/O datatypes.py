cf17a724ac41ce0604f15546b445b2fe32ecaf8f
Tashfeen <muzzamilsubhani62@gmail.com>
2026-09-15 22:53:37 +0500

    Create O datatypes.py

---
+++ zain_day1_I/O datatypes.py
@@ -0,0 +1,34 @@
+print("Hello world")
+
+# str: "jkk" 'jkh' string
+name = "hello world 1234"
+# int: all the positive and negative numbers without decimal
+num = 5
+# float: all the positive and negative numbers with decimal
+numb = 5.6
+# bool: true or fals
+check = True
+check = False
+
+# to check the data type
+print(type(name))
+
+print('hello world')
+
+# Input function
+name = input('enter name')
+print(name)
+print(type(name))
+
+#type casting
+
+#int(): any data type convert into int
+#str(): any data type convert into str
+#float():any data type convert into float
+
+num1 = input("Enter number 1: ")
+num1 = int(num1)
+num2 = input("Enter number 2: ")
+num2 = int(num2)
+print(num1 * num2)
+
